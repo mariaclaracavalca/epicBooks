4b2cc819eabbfbf44ed40956a5499fa2dc39da71
Maria Cavalca <maria.cavalca@xfarm.ag>
2024-07-01 20:43:04 +0200

    struttura - wip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Welcome from './components/Welcome';
-import AllTheBooks from './components/AllTheBooks';
-import MyFooter from './components/MyFooter';
+
 import NavBar from './components/NavBar';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <>
       <NavBar />
       <Welcome />
-      <AllTheBooks />
-      <MyFooter />
     </>
   );
 }
