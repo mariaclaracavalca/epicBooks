00199ecebe11bc38e50941aee7aabd469ad34ae7
Maria Cavalca <maria.cavalca@xfarm.ag>
2024-07-01 07:35:06 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from './components/Navbar';
+import Welcome from './components/Welcome';
+import AllTheBooks from './components/AllTheBooks';
+import MyFooter from './components/Footer';
+import { Container } from 'react-bootstrap'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Benvenuto nel mio primo progetto React!</h1>
-        <p>
-          Questo è il mio primo componente React.
-        </p>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Welcome />
+      <AllTheBooks />
+      <MyFooter />
+    </>
   );
 }
 
